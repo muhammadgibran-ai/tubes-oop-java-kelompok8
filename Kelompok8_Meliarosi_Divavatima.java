6a584368f3cf440638df6bea861d8180ed722361
Muhammad Gibran Ramadhan <muhammad.gibran66@gmail.com>
2025-12-27 21:46:17 +0700

    feat : tambah fitur baru di exception

--- Kelompok8_Meliarosi_Divavatima.java
+++
@@ -1,5 +0,0 @@
-public class Kelompok8_Meliarosi_Divavatima {
-    public static void main (String[] args){
-        System.out.println("Saya Meliarosi Divavatima - 1324041");
-    }
-}
--- syakiroh.java
+++
@@ -1,5 +0,0 @@
-public class syakiroh {
-    public static void main(String[] args) {
-        System.out.println("Syakiroh Suhaimah Agustin - 1324049");
-    }
-}
